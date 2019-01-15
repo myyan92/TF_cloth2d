96c9479616222a290264c7ce320ee789a9feb718
mengyuan <mengyuan@bohg-ws-3.stanford.edu>
2019-01-15 11:43:28 -0800

    update models

--- build_dataset_seq.py
+++ build_dataset_seq.py
@@ -19,15 +19,22 @@ def write_record(image_file, position_file, knot_file, writer):
     writer.write(record.SerializeToString())
 
 
-tfrecords_filename = 'cloth2d_%s_sim_seq.tfrecords'
+tfrecords_filename = 'cloth2d_%s_sim_seq_small.tfrecords'
 img_pattern = "/scr1/mengyuan/data/sim_sequence_bending1000/%04d/%04d_%d.png"
 position_pattern = "/scr1/mengyuan/data/sim_sequence_bending1000/%04d/%04d_%d.txt"
+
+"""parameters
+for big train: run in range(950), t in range(100).
+for medium train: run in range(95), t in range(100).
+for small train: run in range(19), t in range(0,100,2).
+for test: run in range(950,1000), t in range(100).
+"""
 
 train_writer = tf.python_io.TFRecordWriter(tfrecords_filename % ('train'))
 images = []
 positions= []
-for run in range(950):
-    for t in range(100):
+for run in range(19):
+    for t in range(0,100,2):
         images.append(img_pattern % (run, run, t))
         positions.append(position_pattern % (run, run, t))
 idx = np.arange(len(images))
